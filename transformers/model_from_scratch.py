e9e499dbaf28f9da689353a6d6a0934f94276024
Aadit <aadits2004@gmail.com>
2026-03-23 22:57:58 +0530

    pe done

---
+++ transformers/model_from_scratch.py
@@ -0,0 +1,42 @@
+import math
+import torch 
+import torch.nn as nn
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+    
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # create a positional encoding matrix of shape (seq_len, d_model) -> this represents all the tokens(seq_len) and the 512dims of the model
+        pe = torch.zeros(seq_len, d_model)
+
+        # create a vector for representing the positions
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, seq_len, 2).float() * (-math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape(1), :]).requires_grad(False)
+        return self.dropout(x)
+
+    
