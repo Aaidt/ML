723013df609bfefe84d9e35a5d7c01fc3989fc72
Aadit <aadits2004@gmail.com>
2026-05-21 20:46:05 +0530

    created a map for vocab words

--- slm/slm.cpp
+++ slm/slm.cpp
@@ -1,13 +1,13 @@
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <set>
 #include <string>
+#include <vector>
+
+template <typename T> std::vector<T> encode(std::string s) {}
 
 int main() {
-   // std::string filename;
-   // std::cout << "Enter the file name: \n";
-   // std::cin >> filename;
-
    std::ifstream inputFile("shakespeare.txt");
 
    if (!inputFile.is_open()) {
@@ -20,6 +20,18 @@ int main() {
 
    std::set<char> vocab(content.begin(), content.end());
    std::cout << "Vocab size: " << vocab.size() << "\n";
+
+   std::map<int, char> char_to_idx;
+
+   int idx = 0;
+   for (const char &c : vocab) {
+      char_to_idx[idx] = c;
+      idx++;
+   }
+
+   for (const auto &[key, value] : char_to_idx) {
+      std::cout << key << "->" << value << "\n";
+   }
 
    inputFile.close();
 
